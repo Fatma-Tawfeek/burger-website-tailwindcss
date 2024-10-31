9b72622e385615461e3ea37f560c029eae1788e7
fatmatawfeek <fatmatawfeek97@gmail.com>
2024-10-31 11:20:13 +0300

    Create scroll up button & Add dark mode

--- src/main.js
+++ src/main.js
@@ -47,9 +47,61 @@ tabs.forEach((tab) => {
 
 /*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
 
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+const darkMode = () => {
+    html.classList.add("dark");
+    themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+    localStorage.setItem("mode", "dark");
+};
+
+const lightMode = () => {
+    html.classList.remove("dark");
+    themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+    localStorage.setItem("mode", "light");
+};
+
+if (localStorage.getItem("mode") == "dark") {
+    darkMode();
+}
+
+themeBtn.addEventListener("click", (e) => {
+    if (localStorage.getItem("mode") == "light") {
+        darkMode();
+    } else {
+        lightMode();
+    }
+});
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
 
+const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up");
+    /* scroll up */
+    if (this.scrollY >= 250) {
+        scrollUp.classList.remove("-bottom-1/2");
+        scrollUp.classList.add("bottom-4");
+    } else {
+        scrollUp.classList.add("-bottom-1/2");
+        scrollUp.classList.remove("bottom-4");
+    }
+};
+
+window.addEventListener("scroll", scrollUp);
+
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+
+const scrollHeader = () => {
+    const header = document.getElementById("header");
+    /* scroll up */
+    if (this.scrollY >= 250) {
+        header.classList.add("border-b", "border-secondaryColor");
+    } else {
+        header.classList.remove("border-b", "border-secondaryColor");
+    }
+};
+
+window.addEventListener("scroll", scrollHeader);
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
 
